da79d57003df6cfe910be96dc57a71e444bc505c
Matthew Elam <ematew418@gmail.com>
2022-07-19 15:55:32 -0400

    commit tree df87c2db3d22c341f5d819d31ec5a6c981923113

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if (someNumber < 400)
+    return "This one is on me!";
+  if(someNumber > 2500)
+    return "No can do.";
+  if(someNumber > 400 && someNumber < 2000)
+    return "That will be twenty bucks.";
+  if(someNumber <= 2000)
+    return "That will be twenty bucks.";
+  if(someNumber > 2000)
+    return "I will gladly take your thirty bucks.";
+  
+  
+  
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  let result
+return someCity === "NYC" ? "Ok, sounds good." : "No go."
 }
+ 
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tipamount){
+  if(tipamount > 1)
+  return "Thank you so much.";
   // Write your code here!
 }
